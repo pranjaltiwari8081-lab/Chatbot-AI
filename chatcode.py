fb616f4ac8d9a549aac70f3f116916c0a3de2dd6
pranjaltiwari8081-lab <pranjaltiwari8081@gmail.com>
2025-10-23 09:13:37 +0530

    make  syestemic

--- chatcode.py
+++ chatcode.py
@@ -2,26 +2,18 @@ import streamlit as st
 from google import genai
 from dotenv import load_dotenv
 import os
-
-load_dotenv()  # load .env file
-
-# ✅ Directly or via env
+load_dotenv() 
 client = genai.Client(api_key=os.getenv("GOOGLE_API_KEY"))
-
 st.title("Chat with AI Made by Pranjal Tiwari")
-
 if "messages" not in st.session_state:
     st.session_state.messages = []
-
 for msg in st.session_state.messages:
     with st.chat_message(msg["role"]):
         st.markdown(msg["content"])
-
 if prompt := st.chat_input("Type your message..."):
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
-
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         try:
